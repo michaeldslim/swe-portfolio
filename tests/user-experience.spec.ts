3fbe617d97052cde52623e1a84528096800f1fa0
Mike Lim <michaelds.lim@gmail.com>
2026-06-27 16:27:25 -0500

    test: Add initial user experience tests for website navigation

---
+++ tests/user-experience.spec.ts
@@ -0,0 +1,13 @@
+// will be used to test the user experience of the website
+
+// describe('User Experience', () => {
+//   it('should have a clear and concise navigation', () => {
+//     cy.visit('/');
+//     cy.get('nav').should('be.visible');
+//     cy.get('nav').should('have.length', 1);
+//     cy.get('nav').should('have.class', 'nav');
+//     cy.get('nav').should('have.class', 'nav-list');
+//     cy.get('nav').should('have.class', 'nav-list-item');
+//     cy.get('nav').should('have.class', 'nav-list-item-link');
+//   });
+// });
